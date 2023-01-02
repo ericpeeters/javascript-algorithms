68422cf11eb2e0a782295f5239dd09298a01f4ca
Eric Peeters <ericpeeters@live.nl>
2023-01-02 12:15:09 +0100

    Finished stack implementation

--- src/data-structures/stack/Stack.js
+++ src/data-structures/stack/Stack.js
@@ -1,67 +1,63 @@
-import LinkedList from '../linked-list/LinkedList';
-
 export default class Stack {
   constructor() {
-    // We're going to implement Stack based on LinkedList since these
-    // structures are quite similar. Compare push/pop operations of the Stack
-    // with prepend/deleteHead operations of LinkedList.
-    this.linkedList = new LinkedList();
+    this.arr = [];
+  }
+
+  get top() {
+    return this.arr?.[0] ?? null;
   }
 
   /**
    * @return {boolean}
    */
   isEmpty() {
-    // The stack is empty if its linked list doesn't have a head.
-    return !this.linkedList.head;
+    return this.arr.length === 0;
   }
 
   /**
    * @return {*}
    */
   peek() {
-    if (this.isEmpty()) {
-      // If the linked list is empty then there is nothing to peek from.
-      return null;
-    }
-
-    // Just read the value from the start of linked list without deleting it.
-    return this.linkedList.head.value;
+    return this.top;
   }
 
   /**
    * @param {*} value
    */
   push(value) {
-    // Pushing means to lay the value on top of the stack. Therefore let's just add
-    // the new value at the start of the linked list.
-    this.linkedList.prepend(value);
+    this.arr.unshift(value);
+
+    return value;
   }
 
   /**
    * @return {*}
    */
   pop() {
-    // Let's try to delete the first node (the head) from the linked list.
-    // If there is no head (the linked list is empty) just return null.
-    const removedHead = this.linkedList.deleteHead();
-    return removedHead ? removedHead.value : null;
+    if (this.arr.length === 0) {
+      return null;
+    }
+
+    return this.arr.shift();
   }
 
   /**
    * @return {*[]}
    */
   toArray() {
-    return this.linkedList
-      .toArray()
-      .map((linkedListNode) => linkedListNode.value);
+    // We call slice here since we don't want to return a reference to the
+    // actual array we use, since then it might get mutated
+    return this.arr.slice();
   }
 
   /**
    * @param {function} [callback]
    * @return {string}
    */
-  toString(callback) {
-    return this.linkedList.toString(callback);
+  toString(callback = (value) => value) {
+    return this.arr.reduce(
+      (acc, el, index) => `${acc}${index > 0 ? "," : ""}${callback(el)}`,
+      ""
+    );
   }
 }
